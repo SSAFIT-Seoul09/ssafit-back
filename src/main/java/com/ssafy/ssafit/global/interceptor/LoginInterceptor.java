e145f8ab1a1563cb10f4238d2e388857cf7435cb
SUICHO <65757661+whtndls@users.noreply.github.com>
2025-05-16 03:25:38 +0900

    Fix: request에 사용자 정보 저장에서 UserContext에 저장으로 수정

--- src/main/java/com/ssafy/ssafit/global/interceptor/LoginInterceptor.java
+++ src/main/java/com/ssafy/ssafit/global/interceptor/LoginInterceptor.java
@@ -1,11 +1,14 @@
 package com.ssafy.ssafit.global.interceptor;
 
 
+import com.ssafy.ssafit.global.auth.AuthenticatedUser;
+import com.ssafy.ssafit.global.auth.UserContext;
 import com.ssafy.ssafit.global.util.JwtUtil;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -14,6 +17,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
  * 사용자가 요청을 넣을때, webconfig에서 열어놓지 않은 api로 요청을 넣을때 인터셉터가
  * 실행된다.
  */
+@Slf4j(topic = "JWT 토큰 로그인 검증")
 @RequiredArgsConstructor
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
@@ -50,10 +54,17 @@ public class LoginInterceptor implements HandlerInterceptor {
         // 4. 사용자 정보 추출
         Claims claims = jwtUtil.getUserInfoFromToken(token);
 
-        // 5. 사용자 정보 request에 저장 (필요시 다른 곳에서 꺼내 쓸 수 있도록)
-        request.setAttribute("userNiceName", claims.getSubject());  // 사용자의 닉네임 넣어둠
-        request.setAttribute("userRole", claims.get(JwtUtil.AUTHORIZATION_KEY));  // 사용자의 권한 넣어둠.
+        // 5. AuthenticatedUser 객체에 JWT토큰의 사용자 정보 저장
+        AuthenticatedUser authenticatedUser = AuthenticatedUser.of(claims);
+
+        // 6. ThreadLocal에 저장
+        UserContext.setUser(authenticatedUser);
 
         return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserContext.clear(); // 요청 끝나면 반드시 클리어. securityContextHolder가 유저의 정보를 없애버리는 것과 같은 맥락
     }
 }
