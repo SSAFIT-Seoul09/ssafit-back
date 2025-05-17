ffe4788511c08dfbda07fed7e142b4b7fd0decc6
HOJEONGKIMM <khj15263245@gmail.com>
2025-05-17 17:07:59 +0900

    Add : Video 관련 DTO 추가

---
+++ src/main/java/com/ssafy/ssafit/video/dto/VideoResponseDto.java
@@ -0,0 +1,35 @@
+package com.ssafy.ssafit.video.dto;
+
+import com.ssafy.ssafit.video.domain.model.Video;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VideoResponseDto {
+
+    private Long videoId;
+    private String title;
+    private String description;
+    private String part;
+    private String url;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    public static VideoResponseDto toDto(Video video) {
+        return VideoResponseDto.builder()
+                .videoId(video.getId())
+                .title(video.getTitle())
+                .description(video.getDescription())
+                .part(video.getPart().getValue()) // enum → String 변환
+                .url(video.getUrl())
+                .createdAt(video.getCreatedAt())
+                .modifiedAt(video.getModifiedAt())
+                .build();
+    }
+
+}
