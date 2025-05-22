8d9afd31bc8de72f5da81a34a60e6ba9e34b6ef2
SUICHO <65757661+whtndls@users.noreply.github.com>
2025-05-22 09:20:08 +0900

    Style: 일관성 맞춤

--- src/main/java/com/ssafy/ssafit/SsafitApplication.java
+++ src/main/java/com/ssafy/ssafit/SsafitApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = {"com.ssafy.ssafit.user.domain.repository"
+@MapperScan(
+		basePackages = {"com.ssafy.ssafit.user.domain.repository"
 		, "com.ssafy.ssafit.video.domain.repository"
 		, "com.ssafy.ssafit.favorite.domain.repository"
 		, "com.ssafy.ssafit.review.domain.repository"
-		,"com.ssafy.ssafit.comment.domain.repository"
+		, "com.ssafy.ssafit.comment.domain.repository"
 })
 public class SsafitApplication {
 
