944f1adb702f7482d1aa3ff3286da2fc7d0c327f
SUICHO <65757661+whtndls@users.noreply.github.com>
2025-05-24 23:15:49 +0900

    Feat: Swagger 토큰 적용

--- src/main/java/com/ssafy/ssafit/global/config/SwaggerConfig.java
+++ src/main/java/com/ssafy/ssafit/global/config/SwaggerConfig.java
@@ -1,11 +1,24 @@
 package com.ssafy.ssafit.global.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+        security = @SecurityRequirement(name = "bearerAuth") // 기본 적용
+)
+@SecurityScheme(
+        name = "Authorization",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 @Configuration
 public class SwaggerConfig {
 
@@ -19,8 +32,8 @@ public class SwaggerConfig {
 
     private Info info() {
         return new Info()
-                .title("Mongo Basic API")
-                .description("Mongo API reference for developers")
+                .title("Final-PJT SSAFIT for 수인호정")
+                .description("SSAFIT API reference for developers")
                 .version("1.0");
     }
 }
