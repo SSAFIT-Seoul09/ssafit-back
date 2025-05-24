936b789e7d5383bd3c740e5fa8c5456416c39195
SUICHO <65757661+whtndls@users.noreply.github.com>
2025-05-24 23:03:22 +0900

    Feat: Swagger 적용

--- src/main/java/com/ssafy/ssafit/global/config/WebConfig.java
+++ src/main/java/com/ssafy/ssafit/global/config/WebConfig.java
@@ -32,6 +32,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor)
                 .addPathPatterns("/**")
+                .excludePathPatterns("/swagger-ui/**", "/v3/api-docs/**") // Swagger 경로 제외.
                 .excludePathPatterns("/css/**", "/js/**", "/images/**"); // 현재 로그인, 회원가입 요청을 제외하면 모든 요청에 jwt토큰 검증을 진행함.
     }
 
